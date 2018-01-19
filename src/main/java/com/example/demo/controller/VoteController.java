358723d88d79410685381194ece22ef01a45a754
Xu Wei <759974029@qq.com>
2018-01-19 18:15:47 +0800

    vote数据

--- src/main/java/com/example/demo/controller/VoteController.java
+++ src/main/java/com/example/demo/controller/VoteController.java
@@ -2,7 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.domain.Vote;
 import com.example.demo.domain.VoteOption;
+import com.example.demo.domain.VoteRecord;
+import com.example.demo.domain.VoteRecordKey;
 import com.example.demo.mapper.VoteOptionMapper;
+import com.example.demo.mapper.VoteRecordMapper;
 import com.example.demo.service.VoteService;
 import com.example.demo.util.Helper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,21 +20,33 @@ import java.util.Map;
 public class VoteController {
     @Autowired
     private VoteService voteService;
-    @Autowired
-    private VoteOptionMapper voteOptionMapper;
+
+
     @RequestMapping("getVote")
     @ResponseBody
     public Map getAllVote(Vote vote){
        try {
         List<Vote> voteList=voteService.getVote(vote);
            for (int i = 0; i < voteList.size(); i++) {
-               voteList.get(i).setVoteOption(voteOptionMapper.selectListvoteOptionByvoteId(voteList.get(i).getVoteId()));
+               voteList.get(i).setVoteOption(voteService.selectListvoteOptionByvoteId(voteList.get(i).getVoteId()));
            }
            return Helper.getInstance().successJsonResultMap(voteList,"查询成功");
        }catch (Exception e)
        {
            return Helper.getInstance().errorJsonResultMap("SERVER_ERROR","服务器异常:"+e.getCause());
        }
+    }
+    @RequestMapping("vote")
+    @ResponseBody
+    public Map getAllVote(VoteRecord voteRecord){
+        try {
+
+            voteService.addVoteRecord(voteRecord);
+            return Helper.getInstance().successJsonResultMap(null,"");
+        }catch (Exception e)
+        {
+            return Helper.getInstance().errorJsonResultMap("SERVER_ERROR","服务器异常:"+e.getCause());
+        }
     }
 
 }
--- src/main/java/com/example/demo/service/VoteService.java
+++ src/main/java/com/example/demo/service/VoteService.java
@@ -1,7 +1,11 @@
 package com.example.demo.service;
 
 import com.example.demo.domain.Vote;
+import com.example.demo.domain.VoteOption;
+import com.example.demo.domain.VoteRecord;
 import com.example.demo.mapper.VoteMapper;
+import com.example.demo.mapper.VoteOptionMapper;
+import com.example.demo.mapper.VoteRecordMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +15,20 @@ import java.util.List;
 public class VoteService {
     @Autowired
     VoteMapper voteMapper;
+    @Autowired
+    private VoteRecordMapper voteRecordMapper;
+    @Autowired
+    private VoteOptionMapper voteOptionMapper;
 
     public List<Vote> getVote(Vote vote) {
         return voteMapper.getAllVote(vote);
+    }
+
+    public List<VoteOption> selectListvoteOptionByvoteId(Long voteId) {
+        return  voteOptionMapper.selectListvoteOptionByvoteId(voteId);
+    }
+
+    public void addVoteRecord(VoteRecord voteRecord) {
+        voteRecordMapper.insert(voteRecord);
     }
 }
